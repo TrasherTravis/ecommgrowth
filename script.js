9dfc367eeb571edd79bb03f325fb0bc9bc017713
trashertravis <trashertravis@gmail.com>
2023-04-24 22:53:43 +0530

    Call to action button

--- script.js
+++ script.js
@@ -5,3 +5,24 @@
     menuToggle.classList.toggle('active');
     menu.classList.toggle('active');
   });
+
+  $(function() {
+    $( "#button" ).click(function() {
+      $( "#button" ).addClass( "onclic", 250, validate);
+    });
+  
+    function validate() {
+      setTimeout(function() {
+        $( "#button" ).removeClass( "onclic" );
+        $( "#button" ).addClass( "validate", 450, callback );
+      }, 2250 );
+    }
+      function callback() {
+        setTimeout(function() {
+          $( "#button" ).removeClass( "validate" );
+        }, 1250 );
+      }
+    });
+
+
+  
