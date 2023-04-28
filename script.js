15d22109cc865fb0ac6839d9180b47fe2ea476ab
Travis <trashertravis@gmail.com>
2023-04-28 23:24:29 +0530

    Our Services Added

---
+++ script.js
@@ -0,0 +1,28 @@
+  // const menuToggle = document.querySelector('.menu-toggle');
+  // const menu = document.querySelector('.menu');
+
+  // menuToggle.addEventListener('click', () => {
+  //   menuToggle.classList.toggle('active');
+  //   menu.classList.toggle('active');
+  // });
+
+  // $(function() {
+  //   $( "#button" ).click(function() {
+  //     $( "#button" ).addClass( "onclic", 250, validate);
+  //   });
+  
+  //   function validate() {
+  //     setTimeout(function() {
+  //       $( "#button" ).removeClass( "onclic" );
+  //       $( "#button" ).addClass( "validate", 450, callback );
+  //     }, 2250 );
+  //   }
+  //     function callback() {
+  //       setTimeout(function() {
+  //         $( "#button" ).removeClass( "validate" );
+  //       }, 1250 );
+  //     }
+  //   });
+
+
+  
